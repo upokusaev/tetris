830d83208dceaee27531ced8262c982131574188
upokusaev <upokusaev@gmail.com>
2019-11-07 13:49:30 +0400

    Добавил отображение боковой панели

--- src/game.js
+++ src/game.js
@@ -41,7 +41,11 @@ export default class Game {
     }
 
     return {
-      playfield
+      score: this.score,
+      level: this.level,
+      lines: this.lines,
+      nextPiece: this.nextPiece,
+      playfield,
     }
   }
 
